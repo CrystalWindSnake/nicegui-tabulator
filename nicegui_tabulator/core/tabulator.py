375f37a15695e752a395caf6c3697cdf3719d1ee
CrystalWindSnake <36034954+CrystalWindSnake@users.noreply.github.com>
2024-05-10 22:28:44 +0800

    Support for JavaScript function properties (#3)

--- nicegui_tabulator/core/tabulator.py
+++ nicegui_tabulator/core/tabulator.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Callable, Dict
+from typing import Callable, Dict, List, Optional
 from nicegui.element import Element
 from nicegui.events import handle_event
 from nicegui import ui, Client as ng_client
@@ -36,7 +36,14 @@ class Tabulator(Element, component="tabulator.js", libraries=["libs/tabulator.mi
         event: str,
         callback: Callable[..., None],
     ):
-        """ """
+        """
+        Register an event listener for the tabulator table.
+
+        Args:
+            event (str): The name of the event to listen for.
+            callback (Callable[..., None]): The function to call when the event is triggered.
+
+        """
 
         @self.__deferred_task.register
         def _():
@@ -48,7 +55,16 @@ class Tabulator(Element, component="tabulator.js", libraries=["libs/tabulator.mi
     def run_table_method(
         self, name: str, *args, timeout: float = 1, check_interval: float = 0.01
     ) -> AwaitableResponse:
-        """ """
+        """
+        Run a method on the tabulator table.
+
+        Args:
+            name (str): The name of the method to run.
+            *args: The arguments to pass to the method.
+            timeout (float, optional): The maximum time to wait for the method to complete. Defaults to 1.
+            check_interval (float, optional): The interval at which to check if the method has completed. Defaults to 0.01.
+
+        """
         return self.run_method(
             "run_table_method",
             name,
@@ -56,6 +72,80 @@ class Tabulator(Element, component="tabulator.js", libraries=["libs/tabulator.mi
             timeout=timeout,
             check_interval=check_interval,
         )
+
+    def set_columns(self, columns: List[Dict]) -> None:
+        """
+        To replace the current column definitions for all columns in a table.
+
+        @see https://tabulator.info/docs/6.2/columns#replace
+
+        Args:
+            columns (List[Dict]): The list of column definition objects for the table.
+
+        ## Example Usage
+
+        ```python
+        table = Tabulator({...})
+
+        new_columns = [
+            {'title':"Name", 'field':"name"},
+            {'title':"Age", 'field':"age"},
+        ]
+
+        table.set_columns(new_columns)
+        ```
+
+        """
+        self.run_method("setColumns", columns)
+
+    def update_column_definition(self, field: str, definition: Dict) -> None:
+        """
+        Update an existing column definition.
+
+        @see https://tabulator.info/docs/6.2/columns#update
+
+        Args:
+            field (str): The field name of the column you want to update.
+            definition (Dict): The new column definition object for the column.
+
+        ## Example Usage
+        ```python
+        table = Tabulator({...})
+        table.update_column_definition("name", {'title':"Updated Title"})
+        ```
+        """
+        self.run_method("updateColumnDefinition", field, definition)
+
+    def add_column(
+        self,
+        definition: Dict,
+        before: Optional[bool] = None,
+        position: Optional[str] = None,
+    ) -> None:
+        """
+        Add a column to the table.
+
+        @see https://tabulator.info/docs/6.2/columns#add
+
+
+        Args:
+            definition (Dict): The column definition object for the column you want to add.
+            before (Optional[bool], optional): Determines how to position the new column. A value of true will insert the column to the left of existing columns, a value of false will insert it to the right. If a Position argument is supplied then this will determine whether the new colum is inserted before or after this column.
+            position (Optional[str], optional): The field to insert the new column next to, this can be any of the standard column component look up options.
+
+        ## Example Usage
+
+        ```python
+        table = Tabulator({...})
+        table.add_column({'title':"Age", 'field':"age"}, True, "name")
+        ```
+
+        """
+        self.run_table_method("addColumn", definition, before, position)
+
+    def update(self) -> None:
+        super().update()
+        self.run_method("update_table")
 
 
 class DeferredTask:
