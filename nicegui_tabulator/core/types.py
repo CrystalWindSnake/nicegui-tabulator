8fa1f915a62f81b3547043a1b417212aa861720b
CrystalWindSnake <36034954+CrystalWindSnake@users.noreply.github.com>
2024-07-06 01:08:34 +0800

    docs CellSlotProps (#18)

--- nicegui_tabulator/core/types.py
+++ nicegui_tabulator/core/types.py
@@ -11,11 +11,17 @@ if TYPE_CHECKING:
 @dataclass
 class CellSlotProps:
     field: str
+    """The name of the field in the row data."""
     value: Any
+    """The value of the field in the row data."""
     row: Dict
+    """The row data."""
     row_number: int
+    """The position(starting from 1) of the row in the table data."""
     table: Tabulator = dc_field(init=True, repr=False)
+    """The parent Tabulator instance."""
 
     def update_value(self, value):
+        """Updates the value of the field in the row data."""
         data = self.table._props["options"]["data"]
         data[self.row_number - 1].update({self.field: value})
